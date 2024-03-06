527ab65b4747e38e63824e7716fbeaca0adcfeeb
AmyChen159 <144455940+AmyChen159@users.noreply.github.com>
2024-03-06 09:21:36 -0500

    zssszczx

---
+++ vue-api/src/views/OptionsView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            vehicles: []
+        };
+    },
+    mounted: functin () {
+        this.fetchData();
+    },
+    methods: {
+        fetchData: async function () {
+            try {
+                const resilt = await fetch(
+                    `https://data.cityofnewyork.us/resource/h9gi-nx95.json`
+                )
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
