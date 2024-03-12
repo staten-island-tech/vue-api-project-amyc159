9eb2d37eacce2542db150345d42d6fdb76d8fe77
AmyChen159 <144455940+AmyChen159@users.noreply.github.com>
2024-03-12 09:22:38 -0400

    asasas

--- vue-api/src/App.vue
+++ vue-api/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
@@ -15,6 +15,10 @@ import HelloWorld from './components/HelloWorld.vue'
 
   <RouterView />
 </template>
+
+<script>
+
+</script>
 
 <style scoped>
 header {
--- vue-api/src/views/HomeView.vue
+++ vue-api/src/views/HomeView.vue
@@ -2,6 +2,7 @@
 
 <script setup>
 import {ref, onMounted} from 'vue'
+import BarChart from '@/components/BarChart.vue';
 
 const vehicle = ref('')
 
@@ -9,6 +10,10 @@ async function getData() {
   let res = await fetch('https://data.cityofnewyork.us/resource/h9gi-nx95.json')
   let data = await res.json()
   vehicle.value = data.results
+  console.log(vehicle.value)
+  return vehicle
+} catch(error) {
+  console.log(error)
 }
 onMounted(() => {
   getData()
