1a1371f7370ad708a5b18c64e868b0f808ba9c9e
AmyChen159 <144455940+AmyChen159@users.noreply.github.com>
2024-03-19 09:22:22 -0400

    d

--- vue-api/src/views/HomeView.vue
+++ vue-api/src/views/HomeView.vue
@@ -4,7 +4,7 @@
   </div>
 </template>
 
-<script>
+<!-- <script>
 import {ref, onMounted} from 'vue'
 import { Bar } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
@@ -37,7 +37,7 @@ export default {
         } else if(borough === "STATEN ISLAND"){
           //staten island count++
         } else {
-          
+          //no data count++
         }
         }
       })
@@ -55,4 +55,26 @@ export default {
 
 <style scoped>
 
-</style>
+</style> -->
+
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data() {
+    return {
+      chartData: {
+        labels: [ 'Brooklyn', 'Bronx', 'Queens', 'Manhattan', 'Staten Island' ],
+        datasets: [ { data: [40, 20, 12, 3, 19] } ]
+      },
+      chartOptions: {
+        responsive: true
+      }
+    }
+  }
+}
+</script>
