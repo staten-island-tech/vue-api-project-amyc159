2f40dd1035d995cb6efbf9b100b13f7df1ec9643
AmyChen159 <144455940+AmyChen159@users.noreply.github.com>
2024-03-31 23:43:46 -0400

    wee

--- vue-api/src/views/HomeView.vue
+++ vue-api/src/views/HomeView.vue
@@ -1,7 +1,10 @@
 <template>
-<h1>Data of FDNY Line of Duty Deaths</h1>
+<h1 class="header">Data of FDNY Line of Duty Deaths</h1>
+<img src="https://static01.nyt.com/images/2015/02/07/nyregion/07FDNYWEB2/07FDNYWEB2-jumbo.jpg" alt="FDNY Logo" class="logo"
+>
 </template>
 
 <script>
 
 </script>
+
