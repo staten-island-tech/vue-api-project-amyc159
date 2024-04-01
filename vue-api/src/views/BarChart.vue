c318bd744982a5d055dff1b6a43a7b4a60ae32da
AmyChen159 <144455940+AmyChen159@users.noreply.github.com>
2024-03-31 23:15:18 -0400

    WHAT

--- vue-api/src/views/BarChart.vue
+++ vue-api/src/views/BarChart.vue
@@ -94,8 +94,8 @@ import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, Li
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 export default {
-  name: 'PieView',
-  components: { Pie },
+  name: 'BarView',
+  components: { Bar },
   props: {
     chartOptions: {
       type: Object,
